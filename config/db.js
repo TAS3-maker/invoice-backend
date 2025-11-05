c8d38636553c4cf5a6c8f80d7a54991ba63627ec
Amit <amit@gmail.com>
2025-11-05 16:39:12 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// server/config/db.js
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ MongoDB Connected Successfully');
+  } catch (error) {
+    console.error('❌ MongoDB Connection Error:', error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+export default connectDB;
